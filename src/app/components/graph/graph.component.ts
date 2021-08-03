590a6020fb53b24232f6984d4962ba1e1b836605
Polletz <paoletti.riccardo0@gmail.com>
2021-08-03 17:18:26 +0200

    Some minor bug fixes

--- src/app/components/graph/graph.component.ts
+++ src/app/components/graph/graph.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { jsPlumb } from 'jsplumb';
-
+import { Node, Channel } from 'src/app/model/model';
 
 //TODO importare JSPLUMB Community e creare un esempio di grafo con due nodi e un canale
 
@@ -21,12 +21,12 @@ export class GraphComponent implements OnInit {
         // corresponding to the above basic concepts
         source: 'item-8',
         target: 'item-9',
-        anchor: ['Left', 'Right', 'Top', 'Bottom', [0.7, 0, 0, -1], [0.7, 0, 0, -1], [0.3, 1, 0, 1], [0.7, 1, 0, 1]],
+        anchor: ['Left', 'Right', 'Top', 'Bottom', 'TopLeft', 'TopRight', 'BottomLeft', 'BottomRight'],
         connector: 'StateMachine',
         endpoint: 'Blank',
-        overlays: [['Arrow', { width: 8, length: 8, location: 1 }]]//, // overlay
+        overlays: [['Arrow', { width: 8, length: 8, location: 1 }]], // overlay
         // add style
-        // paintStyle: { stroke: '#909399', strokeWidth: 2 } // connector
+        // paintStyle: { stroke: '#909399', strokeWidth: 2 }, // connector
         // endpointStyle: { fill: '#909399', outlineStroke: '#606266', outlineWidth: 1 } // endpoint
       })
     })
