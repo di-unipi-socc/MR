3665675d22446e4550c50a4e9952575f35027a45
jsoldani <jsoldani@gmail.com>
2021-10-19 16:14:48 +0000

    improved backend's reachability

--- Front-End/src/app/services/parse-service/parse.service.ts
+++ Front-End/src/app/services/parse-service/parse.service.ts
@@ -7,35 +7,39 @@ import { Architecture, Channel } from 'src/app/model/model';
   providedIn: 'root'
 })
 export class ParseService {
-  readonly POST_URL = location.host + ':5000/api/parser/route';
-  readonly SET_ARCHITECTURE_URL = location.host + ':5000/api/integration';
-  readonly GET_URL = location.host + ':5000/api/parser/integration';
-  readonly GET_FIXED_URL = location.host + ':5000/api/integration/fixed';
-  readonly GET_ANALYZED_URL = location.host + ':5000/api/integration/analyzed';
+  readonly POST_URL = 'api/parser/route';
+  readonly SET_ARCHITECTURE_URL = 'api/integration';
+  readonly GET_URL = 'api/parser/integration';
+  readonly GET_FIXED_URL = 'api/integration/fixed';
+  readonly GET_ANALYZED_URL = 'api/integration/analyzed';
 
   constructor(private http: HttpClient) { }
 
+  getUrl(endpoint: string) : string {
+    let host: string = "http://" + location.host.split(":")[0]
+    return host + ":8080/" + endpoint
+  }
   postRoute(inputRoute: string): Observable<any> {
-    return this.http.post(this.POST_URL, { "route": inputRoute });
+    return this.http.post(this.getUrl(this.POST_URL), { "route": inputRoute });
   }
 
   getArchitecture(): Observable<Architecture> {
-    return this.http.get<Architecture>(this.GET_URL);
+    return this.http.get<Architecture>(this.getUrl(this.GET_URL));
   }
 
   getFixedArchitecture(): Observable<Architecture> {
-    return this.http.get<Architecture>(this.GET_FIXED_URL);
+    return this.http.get<Architecture>(this.getUrl(this.GET_FIXED_URL));
   }
 
   getAnalyzedArchitecture(): Observable<Architecture> {
-    return this.http.get<Architecture>(this.GET_ANALYZED_URL);
+    return this.http.get<Architecture>(this.getUrl(this.GET_ANALYZED_URL));
   }
 
   setArchitecture(architecture: Architecture) {
     let body = JSON.stringify(architecture);
     console.log("Body: " + body);
     return this.http.post(
-      this.SET_ARCHITECTURE_URL, 
+      this.getUrl(this.SET_ARCHITECTURE_URL), 
       body, 
       {'headers': {
         'content-type': 'application/json'
