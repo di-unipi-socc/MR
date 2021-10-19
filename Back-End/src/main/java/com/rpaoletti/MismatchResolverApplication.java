2a8be054e379e3582f17008f0d7c0b5db58794c7
jsoldani <jsoldani@gmail.com>
2021-10-19 16:18:21 +0000

    removed useless comments

--- Back-End/src/main/java/com/rpaoletti/MismatchResolverApplication.java
+++ Back-End/src/main/java/com/rpaoletti/MismatchResolverApplication.java
@@ -12,35 +12,6 @@ import java.util.List;
 public class MismatchResolverApplication {
 
     public static void main(String[] args) {
-/*
-        NamedType t1 = new NamedType(
-                "t1",
-                null,
-                List.of(
-                        new NamedType("name","string", null, "simple"),
-                        new NamedType("job","string", null, "simple"),
-                        new NamedType("income","double", null, "simple"),
-                        new NamedType("age","double", null, "simple")
-                ),
-                "composite"
-        );
-
-        NamedType t2 = new NamedType(
-                "t2",
-                "empty",
-                List.of(
-                        new NamedType("name","string", null, "simple"),
-                        new NamedType("occupation", null, List.of(
-                                new NamedType("job","string", null, "simple"),
-                                new NamedType("income","double", null, "simple")
-                        ), "composite"),
-                        new NamedType("age","double", null, "simple")
-                ),
-                "composite"
-        );
-
-        System.out.println(Utils.isAdaptable(t1,t2));*/
-
         try {
             SpringApplication.run(MismatchResolverApplication.class, args);
         }catch (Exception e){
